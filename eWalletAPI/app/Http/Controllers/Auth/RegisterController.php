a1a860a1fb8c1f9d49afa9b1436e1a7e1de3089b
Samuel Tan <samueltan3972@gmail.com>
2019-07-02 20:24:32 +0800

    some changes for login and register

--- eWalletAPI/app/Http/Controllers/Auth/RegisterController.php
+++ eWalletAPI/app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255', 'email'],
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'max:255', 'email', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
--- eWalletAPI/bootstrap/app.php
+++ eWalletAPI/bootstrap/app.php
@@ -41,6 +41,10 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+header('Access-Control-Allow-Origin: '.getenv('CORS_ORIGIN'));
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
